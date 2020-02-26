8654b2be3988d7cc72a04ab4d05b2ec2f126e026
Seonghyun, Oh <oh.seonghyun@navercorp.com>
2020-02-26 17:00:39 +0900

    Add test case for java11 action

--- tests/src/test/scala/actionContainers/Java11ActionLoopSourceTests.scala
+++ tests/src/test/scala/actionContainers/Java11ActionLoopSourceTests.scala
@@ -20,6 +20,7 @@ package actionContainers
 import common.WskActorSystem
 import org.junit.runner.RunWith
 import org.scalatest.junit.JUnitRunner
+import spray.json.{JsArray, JsNumber, JsObject}
 
 @RunWith(classOf[JUnitRunner])
 class Java11ActionLoopSourceTests extends JavaActionLoopSourceTests with WskActorSystem {
@@ -28,4 +29,50 @@ class Java11ActionLoopSourceTests extends JavaActionLoopSourceTests with WskActo
 
   behavior of "Java 11 actionloop"
 
+  it should s"support local-variable syntax for lambda parameters (JEP 323)" in {
+    val config = TestConfig(
+      """
+        |package example;
+        |
+        |import com.google.gson.JsonArray;
+        |import com.google.gson.JsonObject;
+        |import java.util.stream.IntStream;
+        |
+        |/**
+        | * Test JEP 323: Local-Variable Syntax for Lambda Parameters
+        | * http://openjdk.java.net/jeps/323
+        | */
+        |public class Java11Jep323Test {
+        |    public static JsonObject main(JsonObject args) throws Exception {
+        |        JsonObject response = new JsonObject();
+        |        JsonArray list = new JsonArray();
+        |
+        |        IntStream.range(1, 5)
+        |                // local-variable syntax
+        |                .filter((var i) -> i % 2 == 0)
+        |                .forEach(list::add);
+        |
+        |        response.add("list", list);
+        |        return response;
+        |    }
+        |}
+      """.stripMargin,
+      main = "example.Java11Jep323Test")
+
+    val (out, err) = withActionContainer() { c =>
+      val (initCode, _) = c.init(initPayload(config.code, config.main))
+      initCode should be(200)
+
+      val response = JsObject("list" -> JsArray(JsNumber(2), JsNumber(4)))
+      val (runCode, out) = c.run(JsObject.empty)
+      runCode should be(200)
+      out should be(Some(response))
+    }
+
+    checkStreams(out, err, {
+      case (o, e) =>
+        o shouldBe empty
+        e shouldBe empty
+    })
+  }
 }
